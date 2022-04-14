a6307c8a0d7ad03f790236008951ac9b9f91f743
Ji-Ha <xodhkd36@naver.com>
2022-04-14 21:56:58 +0900

    init(account): accountService init

--- src/main/java/com/mw/domain/account/service/AccountService.java
+++ src/main/java/com/mw/domain/account/service/AccountService.java
@@ -8,6 +8,6 @@ public class AccountService {
     private final AccountRepository accountRepository;
 
     public AccountService(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository
+        this.accountRepository = accountRepository;
     }
 }
