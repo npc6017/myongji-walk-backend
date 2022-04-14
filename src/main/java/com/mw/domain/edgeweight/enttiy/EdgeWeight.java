4770a66b25a492cd6086d995140381fee148b23c
Ji-Ha <xodhkd36@naver.com>
2022-04-14 21:59:00 +0900

    init(entity): - edgeWeight entity init - node entity init

--- src/main/java/com/mw/domain/edgeweight/enttiy/EdgeWeight.java
+++ src/main/java/com/mw/domain/edgeweight/enttiy/EdgeWeight.java
@@ -1,10 +1,37 @@
 package com.mw.domain.edgeweight.enttiy;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.mw.domain.edge.entity.Edge;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EdgeWeight {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @ManyToOne
+    private Edge edge;
+
+    @Enumerated(EnumType.STRING)
+    private WeightCode weightCode;
+
+    private int weightValue;
+
+    @Builder
+    public EdgeWeight(EdgeWeightDto edgeWeightDto) {
+        this.weightCode = edgeWeightDto.getWeightCode();
+        this.weightValue = edgeWeightDto.getWeightValue();
+    }
+
+    public void setEdge(Edge edge) {
+        this.edge = edge;
+    }
 }
--- src/main/java/com/mw/domain/node/enttiy/Node.java
+++ src/main/java/com/mw/domain/node/enttiy/Node.java
@@ -1,11 +1,30 @@
 package com.mw.domain.node.enttiy;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Node {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false)
+    private String latitude;
+
+    @Column(nullable = false)
+    private String longitude;
+
+    @Builder
+    public Node(NodeDto.NodeInfoDto newNode) {
+        this.latitude = newNode.getLatitude();
+        this.longitude = newNode.getLongitude();
+    }
 }
