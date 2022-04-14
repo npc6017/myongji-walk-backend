a53734392fd33594b936977290d1dde6f425ff4f
Ji-Ha <xodhkd36@naver.com>
2022-04-14 21:59:41 +0900

    feature(edgeRepository): - edge에 연결되어 있는 node 삭제를 위한 메소드 추가

--- src/main/java/com/mw/domain/edge/repository/EdgeRepository.java
+++ src/main/java/com/mw/domain/edge/repository/EdgeRepository.java
@@ -1,9 +1,11 @@
 package com.mw.domain.edge.repository;
 
 import com.mw.domain.edge.entity.Edge;
+import com.mw.domain.node.enttiy.Node;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EdgeRepository extends JpaRepository<Edge, Long> {
+    void deleteAllByStartNodeOrEndNode(Node startNode, Node endNode);
 }
