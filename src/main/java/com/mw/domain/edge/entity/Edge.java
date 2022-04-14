3f580313c727061d90b4d02679ef07a7ea56cd7b
Ji-Ha <xodhkd36@naver.com>
2022-04-14 21:56:30 +0900

    refactor(edge): edge schema 변경

--- src/main/java/com/mw/domain/edge/entity/Edge.java
+++ src/main/java/com/mw/domain/edge/entity/Edge.java
@@ -1,11 +1,42 @@
 package com.mw.domain.edge.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.mw.domain.edgeweight.enttiy.EdgeWeight;
+import com.mw.domain.node.enttiy.Node;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Edge {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @OneToOne
+    private Node startNode;
+
+    @OneToOne
+    private Node endNode;
+
+    @OneToMany
+    private List<EdgeWeight> edgeWeightList;
+
+    @Builder
+    public Edge(Node startNode, Node endNode, List<EdgeWeight> edgeWeightList) {
+        this.startNode = startNode;
+        this.endNode = endNode;
+        this.edgeWeightList = edgeWeightList;
+    }
+
+    public void setEdgeWeightList(List<EdgeWeight> edgeWeightList) {
+        this.edgeWeightList = edgeWeightList;
+    }
 }
