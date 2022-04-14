e51b278a414473fda9d9a2c5578642add5816394
Ji-Ha <xodhkd36@naver.com>
2022-04-14 21:55:54 +0900

    feature: - 새로운 노드 추가 API - 기존 노드 삭제 API - 새로운 엣지 추가 API - 맵 전체 조회 API

--- src/main/java/com/mw/domain/edge/controller/PathFindController.java
+++ src/main/java/com/mw/domain/edge/controller/PathFindController.java
@@ -1,13 +1,55 @@
 package com.mw.domain.edge.controller;
 
+import com.mw.domain.MapDto;
 import com.mw.domain.edge.service.PathFindService;
-import org.springframework.web.bind.annotation.RestController;
+import com.mw.domain.edgeweight.enttiy.EdgeDto;
+import com.mw.domain.node.enttiy.NodeDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/map")
 public class PathFindController {
     private final PathFindService pathFindService;
 
     public PathFindController(PathFindService pathFindService) {
         this.pathFindService = pathFindService;
+    }
+
+    @ApiOperation(value = "새로운 노드 추가")
+    @PostMapping("/node")
+    public ResponseEntity<Long> createNode(@RequestBody NodeDto.NodeInfoDto newNode) {
+        return ResponseEntity.status(HttpStatus.OK).body(pathFindService.createNode(newNode));
+    }
+
+    @ApiOperation(value = "기존 노드 삭제")
+    @DeleteMapping("/edge/{nodeId}")
+    public ResponseEntity<String> deleteNode(@PathVariable long nodeId) {
+        pathFindService.deleteNode(nodeId);
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
+
+    @ApiOperation(value = "새로운 엣지 추가")
+    @PostMapping("/edge")
+    public ResponseEntity<Long> createEDge(@RequestBody EdgeDto.EdgeInfoDto newEdge) {
+        return ResponseEntity.status(HttpStatus.OK).body(pathFindService.createEdge(newEdge));
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "지도 불러오기", content = @Content(schema = @Schema(implementation = MapDto.class)))
+    })
+    @Operation(summary = "맵 전체 조회", description = "맵 전체를 다 긁어옵니다.")
+    @GetMapping
+    public ResponseEntity<MapDto> getMap() {
+        return ResponseEntity.status(HttpStatus.OK).body(pathFindService.getMap());
     }
 }
--- src/main/java/com/mw/domain/edge/service/PathFindService.java
+++ src/main/java/com/mw/domain/edge/service/PathFindService.java
@@ -1,11 +1,24 @@
 package com.mw.domain.edge.service;
 
+import com.mw.domain.MapDto;
+import com.mw.domain.edge.entity.Edge;
 import com.mw.domain.edge.repository.EdgeRepository;
+import com.mw.domain.edgeweight.enttiy.EdgeDto;
+import com.mw.domain.edgeweight.enttiy.EdgeWeight;
+import com.mw.domain.edgeweight.enttiy.EdgeWeightDto;
 import com.mw.domain.edgeweight.repository.EdgeWeightRepository;
+import com.mw.domain.node.enttiy.Node;
+import com.mw.domain.node.enttiy.NodeDto;
 import com.mw.domain.node.repository.NodeRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+
 @Service
+@Transactional
 public class PathFindService {
     private final EdgeRepository edgeRepository;
     private final NodeRepository nodeRepository;
@@ -17,5 +30,65 @@ public class PathFindService {
         this.edgeWeightRepository = edgeWeightRepository;
     }
 
+    public Long createNode(NodeDto.NodeInfoDto newNode) {
+        Node save = nodeRepository.save(Node.builder()
+                .newNode(newNode)
+                .build()
+        );
 
+        return save.getId();
+    }
+
+    public void deleteNode(long nodeId) {
+        Node node = nodeRepository.findById(nodeId).orElseThrow(RuntimeException::new);
+
+        edgeRepository.deleteAllByStartNodeOrEndNode(node, node);
+        nodeRepository.deleteById(nodeId);
+    }
+
+    public Long createEdge(EdgeDto.EdgeInfoDto newEdge) {
+        Node startNode = nodeRepository.findById(newEdge.getStartNode()).orElseThrow(RuntimeException::new);
+        Node endNode = nodeRepository.findById(newEdge.getEndNode()).orElseThrow(RuntimeException::new);
+        Edge edge = Edge.builder()
+                .startNode(startNode)
+                .endNode(endNode)
+                .build();
+
+        List<EdgeWeightDto> edgeWeightDtoList = newEdge.getEdgeWeightDtoList();
+        List<EdgeWeight> edgeWeightList = new ArrayList<>();
+
+        for (EdgeWeightDto e : edgeWeightDtoList) {
+            EdgeWeight edgeWeight = new EdgeWeight(e);
+            edgeWeight.setEdge(edge);
+            edgeWeightList.add(edgeWeight);
+        }
+
+        edge.setEdgeWeightList(edgeWeightList);
+        Edge save = edgeRepository.save(edge);
+        return save.getId();
+    }
+
+    public MapDto getMap() {
+        List<Node> nodeList = nodeRepository.findAll();
+        List<Edge> edgeList = edgeRepository.findAll();
+
+        List<NodeDto.NodeInfoDto> mapNodeList = new ArrayList<>();
+        List<EdgeDto.MapEdgeDto> mapEdgeList = new ArrayList<>();
+
+        for (Node node : nodeList)
+            mapNodeList.add(new NodeDto.NodeInfoDto(node.getLatitude(), node.getLongitude()));
+
+        for (Edge edge : edgeList) {
+            Node startNode = edge.getStartNode();
+            Node endNode = edge.getEndNode();
+
+            mapEdgeList.add(new EdgeDto.MapEdgeDto(new NodeDto.NodeInfoDto(startNode.getLatitude(), startNode.getLongitude()), new NodeDto.NodeInfoDto(endNode.getLatitude(), endNode.getLongitude())));
+        }
+        MapDto build = MapDto.builder()
+                .nodeList(mapNodeList)
+                .edgeList(mapEdgeList)
+                .build();
+
+        return build;
+    }
 }
