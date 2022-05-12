737130cac93c3f9fd1a83ff139b2dcb90b62236d
Ji-Ha <xodhkd36@naver.com>
2022-05-12 23:09:58 +0900

    feat(MapDataUtil) - HashMap에서 데이터를 검색하는 getNodeById를 추가 - HashMap에 노드 데이터 추가하는 기능 추가

--- src/main/java/com/mw/domain/pathfind/MapDataUtil.java
+++ src/main/java/com/mw/domain/pathfind/MapDataUtil.java
@@ -7,16 +7,18 @@ import com.mw.domain.edgeweight.enttiy.WeightCode;
 import com.mw.domain.node.enttiy.Node;
 import com.mw.domain.node.repository.NodeRepository;
 import com.mw.domain.pathfind.entity.MapNode;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
+@Getter
 public class MapDataUtil {
     private final List<Node> nodeArrayList;
     private final List<Edge> edgeArrayList;
+    private final HashMap<Long, Node> nodeHashMap = new HashMap<>();
 
     private final HashMap<Long, List<MapNode>> lowHillGraph = new HashMap<>();
 
@@ -24,6 +26,15 @@ public class MapDataUtil {
         this.nodeArrayList = nodeRepository.findAll();
         this.edgeArrayList = edgeRepository.findAll();
         initLowHillGraph();
+        saveNodeInformation();
+    }
+
+    private void saveNodeInformation() {
+        for (Node node : nodeArrayList) nodeHashMap.put(node.getId(), node);
+    }
+
+    public Node getNodeById(Long id) {
+        return nodeHashMap.get(id);
     }
 
     // TODO: 2022/04/28 이중 for문을 한번만 돌면셔 모든 graph를 만들 수 있도록 리펙토링합시다.
@@ -36,9 +47,17 @@ public class MapDataUtil {
             for (EdgeWeight ew : edgeWeightList) {
                 if(ew.getWeightCode().equals(WeightCode.LOW_HILL)) {
                     if(!lowHillGraph.containsKey(startId)) lowHillGraph.put(startId, new ArrayList<>());
-                    lowHillGraph.get(startId).add(new MapNode(endId, ew.getWeightValue()));
+                    lowHillGraph.get(startId).add(new MapNode(endId, ew.getWeightValue(), edge.getDistance()));
                 }
             }
+        }
+
+        // 거리순 정렬
+        for (Map.Entry<Long, List<MapNode>> entry : lowHillGraph.entrySet()) {
+            List<MapNode> collect = entry.getValue().stream()
+                    .sorted(Comparator.comparing(MapNode::getDistance))
+                    .collect(Collectors.toList());
+            lowHillGraph.put(entry.getKey(), collect);
         }
     }
 }
