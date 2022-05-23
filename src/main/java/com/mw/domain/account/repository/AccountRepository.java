a9c50584681d04bf2c2baff4063c7517041490c0
Ji-Ha <xodhkd36@naver.com>
2022-05-23 16:42:40 +0900

    refactor(Optional): findAccountByEmail에서 Optional 추가

--- src/main/java/com/mw/domain/account/repository/AccountRepository.java
+++ src/main/java/com/mw/domain/account/repository/AccountRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findAccountByEmail(String email);
+    Optional<Account> findAccountByEmail(String email);
 }
--- src/main/java/com/mw/domain/account/service/AccountService.java
+++ src/main/java/com/mw/domain/account/service/AccountService.java
@@ -4,13 +4,13 @@ import com.mw.domain.account.entity.Account;
 import com.mw.domain.account.repository.AccountRepository;
 import com.mw.domain.dto.AccountDto;
 import com.mw.domain.dto.MailDto;
-import com.mw.domain.security.JwtProvider;
+import com.mw.exception.custom.*;
+import com.mw.security.JwtProvider;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -21,17 +21,14 @@ public class AccountService {
 
     @Transactional
     public void signUp(AccountDto.AccountInfoDto accountInfoDto) {
-        Account account = accountRepository.findAccountByEmail(accountInfoDto.getEmail());
-
-        // 존재하는 account가 없다는 예외
-        if(account == null) throw new RuntimeException();
-
+        Account account = accountRepository.findAccountByEmail(accountInfoDto.getEmail()).orElseThrow(AccountNotFoundException::new);
         account.inputDetails(accountInfoDto.getPassword());
     }
 
     public AccountDto.TokenDto signIn(AccountDto.AccountInfoDto accountInfoDto) {
-        Account account = accountRepository.findAccountByEmail(accountInfoDto.getEmail());
-        if(!account.validatePassword(accountInfoDto.getPassword())) throw new RuntimeException();
+        Account account = accountRepository.findAccountByEmail(accountInfoDto.getEmail()).orElseThrow(AccountNotFoundException::new);
+
+        if (!account.validatePassword(accountInfoDto.getPassword())) throw new PasswordIncorrectException();
 
         return AccountDto.TokenDto.builder()
                 .accessToken(jwtProvider.createAccessToken(account))
@@ -40,38 +37,31 @@ public class AccountService {
 
 
     public void validateCode(AccountDto.AccountCodeDto accountCodeDto) {
-        Account account = accountRepository.findAccountByEmail(accountCodeDto.getEmail());
-        // 존재하는 account가 없다는 예외
-        if(account == null) throw new RuntimeException();
-
-        // code가 다르다는 예외
-        if(!account.validateCode(accountCodeDto.getCode())) throw new RuntimeException();
+        Account account = accountRepository.findAccountByEmail(accountCodeDto.getEmail()).orElseThrow(AccountNotFoundException::new);
+        if (!account.validateCode(accountCodeDto.getCode())) throw new CodeNotEqualException();
     }
 
     @Transactional
     public void validateEmail(AccountDto.AccountEmailDto accountEmailDto) {
         String email = accountEmailDto.getEmail();
+        Account account;
         validateMyoungJiPeople(email);
 
-        Account account = accountRepository.findAccountByEmail(accountEmailDto.getEmail());
-        if (account != null) throw new RuntimeException();
+        Optional<Account> accountOptional = accountRepository.findAccountByEmail(accountEmailDto.getEmail());
+        if (accountOptional.isPresent()) throw new AccountAlreadyExistException();
         else account = Account.builder()
                 .email(accountEmailDto.getEmail())
                 .build();
 
         Integer code = account.randomizeCode();
 
-        // 이메일로 코드 보내기.
         mailService.mailSend(new MailDto(account.getEmail(), code.toString()));
-
         accountRepository.save(account);
     }
 
     private void validateMyoungJiPeople(String email) {
         String[] split = email.split("@");
-        // 이메일형식에 안맞는거임 예외 던지기
-        if(split.length != 2) throw new RuntimeException();
-        // 명지대학교 학생이 아닌거야 예외 던지기
-        if(!split[1].equals("mju.ac.kr")) throw new RuntimeException();
+        if (split.length != 2) throw new NotMyoungJiMemberException();
+        if (!split[1].equals("mju.ac.kr")) throw new NotInEmailFormatException();
     }
 }
